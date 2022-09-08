4f2ec0f88f2342b73fc1d9d75a40bf095e9e4819
Aris Apost <ariser6@hotmail.com>
2022-09-08 06:32:30 +0000

    Initial commit from PHPSandbox

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome to PHPSandbox</title>
+    <link rel="icon" href="https://phpsandbox.io/assets/img/brand/phpsandbox.png">
+    <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,400;0,500;0,531;0,600;0,700;0,800;0,900;1,400;1,500;1,531;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="h-screen bg-white flex items-center justify-center font-sans text-gray-700" style="font-family: 'Jost', sans-serif">
+   <?php 
+
+    function mycd(){
+        return "bbb";
+    }
+   echo mycd();
+   ?>
+</div>
+</body>
+</html>
